509418aedf3fa149c4fbae374ec04ad5b52a7ab8
Umanets Stanislav <stasuma@gmail.com>
2023-05-18 19:12:32 -0400

    Add common-application and order-application modules

---
+++ common/common-application/src/main/java/com/food/ordering/system/application/handler/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.food.ordering.system.application.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ErrorDTO.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message("Unexpected error!")
+                .build();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = {ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleException(ValidationException exception) {
+        ErrorDTO errorDTO;
+        if (exception instanceof ConstraintViolationException) {
+            String violations = extractViolationsFromException((ConstraintViolationException) exception);
+            log.error(violations, exception);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(violations)
+                    .build();
+        } else {
+            String exceptionMessage = exception.getMessage();
+            log.error(exceptionMessage, exception);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(exceptionMessage)
+                    .build();
+        }
+
+        return errorDTO;
+    }
+
+    private String extractViolationsFromException(ConstraintViolationException exception) {
+        return exception.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("--"));
+    }
+}
